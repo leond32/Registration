bdf51a34f8e62123fbca732d1cbfecf294982f83
Linus Eck <ge75puk@mytum.de>
2024-06-07 16:33:02 +0200

    updated BaselineAFhQ for experiments

--- utils/BaselineAFHQ.py
+++ utils/BaselineAFHQ.py
@@ -1,11 +1,9 @@
 import numpy as np
 import torch
-from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
 from FreeFormDeformation import DeformationLayer
 from deepali.core import functional as U
-from tqdm import tqdm
 import random
 from diffusion_unet import Unet
 from torch import nn, optim
@@ -15,15 +13,19 @@ from torch.cuda.amp import GradScaler, autocast
 from PIL import Image
 import torchvision.transforms.functional as F
 import sys
-from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
 
+############################################################################################################
 
 def get_mean_std(images):
     mean = torch.mean(images.float())
     std = torch.std(images.float())
     return mean.item(), std.item()
 
-
+############################################################################################################
 
 class CustomDataset(Dataset):
     def __init__(self, image_paths, transform=None, device = "cpu"):
@@ -85,7 +87,7 @@ class CustomDataset(Dataset):
 
         return stacked_image, deformation_field
     
-
+############################################################################################################
 
 def early_stopping(val_losses, patience=5):
     if len(val_losses) < patience:
@@ -95,7 +97,20 @@ def early_stopping(val_losses, patience=5):
             return False
     return True
 
-def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs, device, log_dir='afhq_logs', patience=5):
+############################################################################################################        
+
+def gradient_regularization_loss(deformation_field):
+    # compute the gradient in x and y direction
+    dx = deformation_field[:, :, 1:] - deformation_field[:, :, :-1]
+    dy = deformation_field[:, 1:, :] - deformation_field[:, :-1, :]
+    
+    # compute the mean of the absolute values of the gradients
+    loss = torch.mean(torch.abs(dx)) + torch.mean(torch.abs(dy))
+    return loss
+
+############################################################################################################        
+
+def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs, device, log_dir='afhq_logs', patience=5, alpha=0.05):
     # Create directories if they don't exist
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -105,7 +120,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs,
     val_losses = []
 
     # Open CSV file for logging
-    csv_path = os.path.join(log_dir, 'losses_128.csv')
+    csv_path = os.path.join(log_dir, 'losses.csv')
     with open(csv_path, 'w') as f:
         f.write('epoch,train_loss,val_loss\n')
 
@@ -122,7 +137,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs,
             
             optimizer.zero_grad()
             outputs = model(images)
-            loss = criterion(outputs, deformation_field)
+            loss = criterion(outputs, deformation_field) + alpha*gradient_regularization_loss(outputs)
             train_loss += loss.item()
             loss.backward()
             optimizer.step()
@@ -138,8 +153,8 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs,
                 deformation_field = deformation_field.to(device)
                 
                 outputs = model(images)
-                batch_loss = criterion(outputs, deformation_field).item()
-                val_loss += batch_loss
+                batch_loss = criterion(outputs, deformation_field) + alpha*gradient_regularization_loss(outputs)
+                val_loss += batch_loss.item()
                 
         avg_val_loss = val_loss / len(val_loader)
         val_losses.append(avg_val_loss)
@@ -157,7 +172,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs,
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
             best_epoch = epoch + 1
-            torch.save(model.state_dict(), os.path.join(log_dir, 'best_model_128.pth'))
+            torch.save(model.state_dict(), os.path.join(log_dir, 'best_model.pth'))
             print(f'Model saved at epoch {epoch+1} with validation loss {avg_val_loss:.8f}')
         
 
@@ -166,6 +181,8 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs,
             print('Early stopping...')
             break          
     
+############################################################################################################    
+
 def get_image_paths(root_dir): 
     
     image_paths = []
@@ -176,30 +193,162 @@ def get_image_paths(root_dir):
                 if filename.endswith(".jpg") or filename.endswith(".png"):
                     image_paths.append(os.path.join(category_dir, filename))   
     return image_paths
+
+
+############################################################################################################
+
+def deform_image(deformed_image: torch.Tensor, displacement_field: torch.Tensor, device) -> torch.Tensor:
+    """
+    Deform a grayscale image using the given displacement field.
+
+    Args:
+        deformed_image (torch.Tensor): Grayscale image of shape (H, W).
+        displacement_field (torch.Tensor): Displacement field of shape (2, H, W).
+
+    Returns:
+        torch.Tensor: Deformed image of shape (H, W).
+    """
+    # Ensure the input image and displacement field are on the same device
+    deformed_image = deformed_image.to(device)
+    displacement_field = displacement_field.to(device)
+    
+    # invert the displacement field
+    displacement_field = -displacement_field
+
+    # Create grid coordinates
+    H, W = deformed_image.shape
+    grid_y, grid_x = torch.meshgrid(torch.arange(H), torch.arange(W))
+    grid = torch.stack([grid_x, grid_y], dim=0).float().to(device)  # Shape: (2, H, W)
+
+    # Add displacement field to grid
+    new_grid = grid
+    new_grid = new_grid.permute(1, 2, 0).unsqueeze(0)  # Shape: (1, H, W, 2)
+
+    # Normalize grid values to be in the range [-1, 1]
+    new_grid[..., 0] = 2.0 * new_grid[..., 0] / (W - 1) - 1.0
+    new_grid[..., 1] = 2.0 * new_grid[..., 1] / (H - 1) - 1.0
+    new_grid = new_grid + displacement_field.permute(1, 2, 0).unsqueeze(0)
+
+    # Interpolate original image using the new grid
+    deformed_image = deformed_image.unsqueeze(0).unsqueeze(0)  # Shape: (1, 1, H, W)
+    deformed_image = F.grid_sample(deformed_image, new_grid, mode='bilinear', padding_mode='border')
+
+    return deformed_image.squeeze(0).squeeze(0)
+
+############################################################################################################
+
+def normalize_image(image):
+    return (image - image.min()) / (image.max() - image.min())
+
+############################################################################################################
+
+def compute_metrics(model, val_loader, device):
+    # Compute similarity measures of image and redeformed image from validation data (SSIM, PSNR, MSE, L1)
+    model.eval()
+    with torch.no_grad():
+        avg_ssim = 0
+        avg_psnr = 0
+        avg_mse = 0
+        avg_l1 = 0
+
+        total_images = 0
+
+        for i, (images, deformation_field) in enumerate(val_loader):
+            # Move validation data to the device
+            images = images.float().to(device)
+            deformation_field = deformation_field.to(device)
+        
+            outputs = model(images)
+        
+            batch_size = images.size(0)
+            total_images += batch_size
+        
+            for j in range(batch_size):
+                redeformed_image = deform_image(images[j, 1], outputs[j], device)
+            
+                # Normalize images before SSIM calculation
+                img_0_norm = normalize_image(images[j, 0].cpu().numpy())
+                redeformed_img_norm = normalize_image(redeformed_image.cpu().numpy())
+            
+                # SSIM
+                ssim_value = ssim(img_0_norm, redeformed_img_norm, data_range=1.0)
+                avg_ssim += ssim_value
+            
+                # MSE
+                mse_value = F.mse_loss(images[j, 0], redeformed_image)
+                avg_mse += mse_value.item()
+            
+                # L1
+                l1_value = F.l1_loss(images[j, 0], redeformed_image)
+                avg_l1 += l1_value.item()
+            
+                # PSNR
+                psnr_value = 10 * torch.log10(1 / mse_value)
+                avg_psnr += psnr_value.item()
+    
+        # Normalize by the total number of images processed
+        avg_ssim /= total_images
+        avg_psnr /= total_images
+        avg_mse /= total_images
+        avg_l1 /= total_images
+        
+        return avg_ssim, avg_psnr, avg_mse, avg_l1
+    
+############################################################################################################
             
 def main():
     
+    # Set the device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # Define the paths to the training and validation data
     train_data_path = '/vol/aimspace/projects/practical_SoSe24/registration_group/datasets/Animal_Faces/afhq/train'
     val_data_path = '/vol/aimspace/projects/practical_SoSe24/registration_group/datasets/Animal_Faces/afhq/val'
-
+    
+    # Define the paths to save the logs and the best model	
+    log_dir = '/vol/aimspace/projects/practical_SoSe24/registration_group/AFHQ_Experiments' # Change if you dont train on AFHQ
+    experiment_name = 'Experiment_01' # Change this to a different name for each experiment 
+    experiment_dir = os.path.join(log_dir, experiment_name)
+    best_model_path = os.path.join(experiment_dir,'best_model.pth')
+    
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if not os.path.exists(experiment_dir):
+        os.makedirs(experiment_dir)
+    
+    # Get the image paths for dynamic dataset creation    
     train_images_paths = get_image_paths(train_data_path)
     val_images_paths = get_image_paths(val_data_path)
     
-    mean = 113
-    std = 61
+    # Define the hyperparameters
+    hparams = {
+        'mean': 113,
+        'std': 61,
+        'n_epochs': 20,
+        'batch_size': 16,
+        'lr': 0.001,
+        'weight_decay': 1e-5,
+        'patience': 10,
+        'alpha': 0.005
+    }
     
-    train_dataset = CustomDataset(train_images_paths, transform=transforms.Compose([transforms.Normalize(mean=[mean], std=[std])]), device=device)
-    val_dataset = CustomDataset(val_images_paths, transform=transforms.Compose([transforms.Normalize(mean=[mean], std=[std])]), device=device)
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=True)
+    # add a configuration file to save the hyperparameters in the experiment directory
+    with open(os.path.join(experiment_dir, 'config.txt'), 'w') as f:
+        for key, value in hparams.items():
+            f.write(f'{key}: {value}\n')
+    
+    # Create the datasets and dataloaders
+    train_dataset = CustomDataset(train_images_paths, transform=transforms.Compose([transforms.Normalize(mean=[hparams['mean']], std=[hparams['std']])]), device=device)
+    val_dataset = CustomDataset(val_images_paths, transform=transforms.Compose([transforms.Normalize(mean=[hparams['mean']], std=[hparams['std']])]), device=device)
+    train_loader = DataLoader(train_dataset, batch_size=hparams['batch_size'], shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=hparams['batch_size'], shuffle=True)
 
+    # Define the model
     model = Unet(
         dim=8,
         init_dim=None,
         out_dim=2,
-        dim_mults=(1, 2, 4, 8, 16),
+        dim_mults=(1, 2, 4, 8),
         channels=2,
         resnet_block_groups=8,
         learned_variance=False,
@@ -209,20 +358,44 @@ def main():
     )
 
     model.to(device)
-    # Check if weights file exists
-    if os.path.isfile('/vol/aimspace/projects/practical_SoSe24/registration_group/model_weights/model_weights_afhq_128.pth'):
-        model.load_state_dict(torch.load('/vol/aimspace/projects/practical_SoSe24/registration_group/model_weights/model_weights_afhq_128.pth', map_location=device))
-
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.003, weight_decay=1e-5)
-
-    n_epochs = 30
-    train_model(model, train_loader, val_loader, criterion, optimizer, n_epochs, device, log_dir='afhq_logs', patience = 5)
     
-    # save (update the number of epochs in name)
-   
-    torch.save(model.state_dict(), '/vol/aimspace/projects/practical_SoSe24/registration_group/model_weights/model_weights_afhq_128.pth')
+    # Check if weights file exists
+    if os.path.isfile(best_model_path):
+        model.load_state_dict(torch.load(best_model_path, map_location=device))
 
+    # Define the loss function and optimizer
+    criterion = nn.MSELoss() + gradient_regularization_loss
+    optimizer = optim.Adam(model.parameters(), lr=hparams['lr'], weight_decay=hparams['weight_decay'])
+
+    # Train the model
+    train_model(model, train_loader, val_loader, criterion, optimizer, hparams['n_epochs'], device, log_dir=experiment_dir, patience = hparams['patience'], alpha=hparams['alpha'])
+
+    # after training, save an image of the loss and calculate some metrics on the validatuion set
+    if not os.path.exists(os.path.join(experiment_dir, 'images')):
+        os.makedirs(os.path.join(experiment_dir, 'images'))
+    if not os.path.exists(os.path.join(experiment_dir, 'metrics')):
+        os.makedirs(os.path.join(experiment_dir, 'metrics'))
+        
+    # create the loss plot from the csv file and save it
+    csv_path = os.path.join(experiment_dir, 'losses.csv')
+    df = pd.read_csv(csv_path)
+    plt.plot(df['epoch'], df['train_loss'], label='Train Loss')
+    plt.plot(df['epoch'], df['val_loss'], label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join(experiment_dir, 'images', 'loss_plot.png'))
+    plt.close()
+    
+    # calculate metrics on the validation set and save them in a txt file
+    avg_ssim, avg_psnr, avg_mse, avg_l1 = compute_metrics(model, val_loader, device)
+    with open(os.path.join(experiment_dir, 'metrics', 'metrics.txt'), 'w') as f:
+        f.write(f'Average SSIM: {avg_ssim}\n')
+        f.write(f'Average PSNR: {avg_psnr}\n')
+        f.write(f'Average MSE: {avg_mse}\n')
+        f.write(f'Average L1: {avg_l1}\n')
+    
+    
 if __name__ == "__main__":
     main()
 
